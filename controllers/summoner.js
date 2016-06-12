e1bd5b618355032a194267bc29de450c825bfb53
Krzysztof Kraszewski <krzysztof@kraszewski.me>
2016-06-12 14:26:22 +0200

    getting potential premades from match working

--- controllers/summoner.js
+++ controllers/summoner.js
@@ -5,7 +5,7 @@ var express = require('express'),
 
 router.get('/summoner/:region/:id', function(req, res) {
   var region = req.params.region,
-      id = req.params.id;
+      id = Number(req.params.id);
   request('https://' + region + '.api.pvp.net/api/lol/' + region + '/v1.4/summoner/' + id + '?api_key=' 
   + process.env.KEY, function(e, r, b) {
     if(e) {
@@ -14,7 +14,12 @@ router.get('/summoner/:region/:id', function(req, res) {
       if(r.statusCode == 200) {
         b = JSON.parse(b);
         getMatchlist(id, region, function(e, matches) {
-          res.json(matches.length);
+          getMatch(matches[0].matchId, region, id, {
+            ids: {},
+            games: 0
+          }, function(e, data) {
+            res.json(data);
+          });
         });
       } else {
         log.error(r.statusCode);
@@ -35,6 +40,52 @@ function getMatchlist(id, region, cb) {
         b = JSON.parse(b);
         var matches = b.matches;
         cb(null, matches);
+      } else {
+        log.error(r.statusCode);
+      }
+    }
+  });
+}
+
+function getMatch(matchId, region, id, data, cb) {
+  request('https://' + region + '.api.pvp.net/api/lol/' + region + '/v2.2/match/'
+  + matchId + '?api_key=' + process.env.KEY, function(e, r, b) {
+    if (e) {
+      log.error(e);
+    } else {
+      if(r.statusCode == 200) {
+        var match = JSON.parse(b),
+            checkedParticipantId,
+            checkedParticipantTeam;
+        // find checked participant's id
+        for(var i = 0, len = match.participantIdentities.length; i < len; i++) {
+          if(match.participantIdentities[i].player.summonerId == id) {
+            checkedParticipantId = match.participantIdentities[i].participantId;
+          }
+        }
+        // find checked participant's team
+        for(var i = 0, len = match.participants.length; i < len; i++) {
+          if(match.participants[i].participantId == checkedParticipantId) {
+            checkedParticipantTeam = match.participants[i].teamId;
+          }
+        }
+        // collect all participants with all needed data to array
+        for(var i = 0, len = match.participantIdentities.length; i < len; i++) {
+          for(var j = 0, len = match.participants.length; j < len; j++) {
+            if(match.participantIdentities[i].participantId == match.participants[j].participantId
+            && match.participants[j].teamId == checkedParticipantTeam
+            && match.participantIdentities[i].participantId != checkedParticipantId) {
+              if(data.ids[match.participantIdentities[i].player.summonerId]) {
+                data.ids[match.participantIdentities[i].player.summonerId]++;
+              } else {
+                data.ids[match.participantIdentities[i].player.summonerId] = 1;
+              }
+            }
+          }
+        }
+        data.games++;
+
+        cb(null, data);
       } else {
         log.error(r.statusCode);
       }
